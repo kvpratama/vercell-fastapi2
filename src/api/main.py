82a4b885df7d146a2fd22347d5ef7f95639761ac
kvpratama <kvpratama@gmail.com>
2025-08-06 15:34:22 +0900

    Update main.py

--- src/api/main.py
+++ src/api/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from ..ai.graph import workflow_app
 
 app = FastAPI()
 
 @app.get("/")
 async def health_check():
     return "The health check is successful."
+
+@app.get("/chat")
+async def chat():
+    return workflow_app.invoke({"messages": []})
